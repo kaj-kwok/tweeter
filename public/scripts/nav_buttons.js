5ac471ad5ad9aae7a40a65c63e8244ac745234bb
Ryan Kwok <kaj.kwok@gmail.com>
2022-03-30 22:08:45 -0700

    added upscroll button + hide nav when scrolling

--- public/scripts/nav_buttons.js
+++ public/scripts/nav_buttons.js
@@ -1,11 +1,30 @@
 $(document).ready(() => {
+
+  //new tweet button, reset view and open textarea
   $('.nav_text_arrow').on("click", (e) => {
     if($('html').scrollTop() === 0){
       $('.new-tweet').slideToggle()
     } else {
       $('html').animate({scrollTop: '0'}, 1000);
-      $('.new-tweet').show()
+      $('.new-tweet').show();
     }
-  })
+  });
 
+  //trigger event to show scroll to top if user scrolls past certain point
+  $(window).on("scroll", () => {
+    if($('html').scrollTop() > 30) {
+      $('#scrollToTop').show();
+      $('nav').hide();
+    }
+    else if($('html').scrollTop() === 0){
+      $('#scrollToTop').hide();
+      $('nav').show();
+    }
+  });
+
+  //event button scroll to top
+  $('#scrollToTop').on("click", () => {
+    $('html').animate({scrollTop: '0'}, 1000);
+    $('.new-tweet').show();
+  });
 });
